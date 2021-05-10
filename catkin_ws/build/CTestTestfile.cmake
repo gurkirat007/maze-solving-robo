b5bca4e354d91d15b458a8d6d54f5b6a013ffbd4
Gurkirat Singh <gs832025@student.nitw.ac.in>
2021-05-10 17:06:42 +0530

    first commit

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/vm-gs007/two_wheel_robo/catkin_ws/src
+# Build directory: /home/vm-gs007/two_wheel_robo/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("motion_plan")
---
+++ simulation_ws/build/my_worlds/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/vm-gs007/two_wheel_robo/simulation_ws/src/my_worlds
+# Build directory: /home/vm-gs007/two_wheel_robo/simulation_ws/build/my_worlds
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
